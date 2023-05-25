33fe463a2f5922de719704ff6b532818f76da72e
KalenXsosa <j.haywoom@gmail.com>
2023-05-25 11:20:45 -0700

    kol

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of a short int: %ld byte(s)\n", sizeof(short int));
-printf("Size of a int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("size of float: %ld byte(s)
+printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of short int: %ld byte(s)\n", sizeof(short int));
+printf("Size of int: %ld byte(s)\n", sizeof(int));
+printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of float: %ld byte(s)\n", sizeof(float));
+printf("Size of double: %ld byte(s)\n", sizeof(double));
+printf("Size of long double: %ld byte(s)\n", sizeof(long double));
 return (0);
 }
