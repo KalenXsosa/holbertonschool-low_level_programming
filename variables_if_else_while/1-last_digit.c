4e82421730182bf251c3f264c51db85fc26c4c2f
KalenXsosa <j.haywoom@gmail.com>
2023-05-29 07:54:58 -0700

    kiiii

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,18 +17,18 @@ int lastdigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastdigit = n % 10;
-printf("last digit of %d is ", n);
+printf("Last digit of %d is ", n);
 if (n > 5)
 {
-printf("%d last digit  is greater then 5\n", lastdigit);
+printf("%d Last digit and is greater then 5\n", lastdigit);
 }
 else if (n == 0)
 {
-printf("%d last digit is zero\n", lastdigit);
+printf("%d Last digit and is zero\n", lastdigit);
 }
 else
 {
-printf("%d last digit is less then 6\n", lastdigit);
+printf("%d Last digit and is less then 6\n", lastdigit);
 }
 return (0);
 }
