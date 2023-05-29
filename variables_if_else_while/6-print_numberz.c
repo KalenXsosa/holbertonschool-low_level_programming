c2fc3380b5dafe03ad7f28a57ef8aab14eae145c
KalenXsosa <j.haywoom@gmail.com>
2023-05-29 11:17:05 -0700

    lolool

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - printing single digits
+* Return: always 0
+*/
+int main(void)
+{
+int h;
+h = '0'
+for (h = 0; h < 10; h++)
+putchar("%d", h);
+putchar('\n')
+return (0)
+}
+
+
+
