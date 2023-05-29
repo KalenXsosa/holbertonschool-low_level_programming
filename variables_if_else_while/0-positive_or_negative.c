05ae228c2f6f1541eb31492c5355b49f16ea1c2b
KalenXsosa <j.haywoom@gmail.com>
2023-05-29 06:10:55 -0700

    kill

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -27,7 +27,7 @@ else if (n == 0)
 printf("%d is zero\n", n);
 }
 else
-{	
+{i
 printf("%d is negative\n", n);
 }
 return (0);
