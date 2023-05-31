fb7458bbdad24ff37297de16c8f37df5acf63355
KalenXsosa <j.haywoom@gmail.com>
2023-05-31 10:12:43 -0700

    cboy

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code.
- *
+ * main - checks uppercase
+ * @c: c checks upper letter
  * Return: Always 0.
  */
-int main(void)
+int _isupper(int c);
+
 {
+
+for (c = 'A'; c <= 'Z'; c++)
+}
+k = _islower(c);
+_putchar(c);
+	}
+return (0);
+
+}
