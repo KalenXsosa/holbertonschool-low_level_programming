72ebdc97d483056fe93418e9650d97ca7501faac
KalenXsosa <j.haywoom@gmail.com>
2023-05-31 14:34:33 -0700

    kill

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply
+ *@b: function
+ *@a: colon
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -24,5 +24,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
