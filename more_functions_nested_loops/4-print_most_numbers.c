b86d3b9388290f3bc3c9127d11e325c415862415
KalenXsosa <j.haywoom@gmail.com>
2023-06-01 06:39:23 -0700

    kio

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,12 @@ void print_most_numbers(void)
 
 char k;
 
- for (k = 0; k <= 9; k++)
- {
- if (k != 2 && k != 4 )
- {
+for (k = 0; k <= 9; k++)
+
+{
+
+if ((k != 2) && (k != 4))
+{
 _putchar('0' + k);
 }
 _putchar('\n');
