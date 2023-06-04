6a6856a49e885c44c90e32e085783e8dc15bfc53
KalenXsosa <j.haywoom@gmail.com>
2023-06-04 11:49:56 -0700

    lol

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,16 +10,18 @@ void print_diagonal(int n)
 	int k, f;
 
 	if (n <= 0)
-	
+	{	
 	_putchar('\n');
-	
+	}
+	else
+	{
 	
 	for (k = 0; k < n; k++)
 	{
 
 
 	for (f = 0; f < k; f++)
-	
+	{
 
 	_putchar(' ');
 	}
