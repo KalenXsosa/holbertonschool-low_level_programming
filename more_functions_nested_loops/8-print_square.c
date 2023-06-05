f2cd995d9d9ba874bfbf443757f744aebd7eda76
KalenXsosa <j.haywoom@gmail.com>
2023-06-05 15:36:23 -0700

    ghght

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,15 @@ void print_square(int size)
 {
 	int row, column;
 
+	if (size <= 0)
+	{
+
+		_putchar('\n');
+	}
+
+	else
+	{
+
 	for (row = 0; row < size; row++)
 	{
 		for (column = 0; column < size; column++)
@@ -17,7 +26,7 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-
+	}
 	_putchar('#');
 
 	_putchar('\n');
