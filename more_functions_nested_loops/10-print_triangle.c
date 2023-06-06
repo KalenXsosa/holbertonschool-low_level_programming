ba7c031a5bb86150eca49fea89134887056e604c
KalenXsosa <j.haywoom@gmail.com>
2023-06-05 17:47:59 -0700

    kikiii

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ *
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+
+int o, u, p;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+
+	else
+	{
+		o = 1;
+		while (o <= size)
+
+		{
+			u = size - o;
+			while (u > 0)
+			{
+			_putchar(' ');
+			u--;
+			}
+
+			p = o;
+			while (p > 0)
+			{
+			_putchar('#');
+			p--;
+			}
+			_putchar('\n');
+			o++;
+		}
+	}
+
+
+}
+
+
+
