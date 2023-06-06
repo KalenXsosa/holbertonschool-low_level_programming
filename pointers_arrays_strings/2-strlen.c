d31a7fdf4ea7510cbe0200e257a9815e5c93e418
KalenXsosa <j.haywoom@gmail.com>
2023-06-05 20:34:34 -0700

    kiii

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ * @s: pop
+ * Return: Always 0.
+ */
+int _strlen(char *s);
+{
+	char *str;
+	int len;
+
+	str = "my first strlen";
+	len = strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
+
