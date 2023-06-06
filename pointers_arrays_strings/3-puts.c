36b8f75714fda6733faf04674055f76cffa99433
KalenXsosa <j.haywoom@gmail.com>
2023-06-05 20:57:55 -0700

    kkiii

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: hi
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
