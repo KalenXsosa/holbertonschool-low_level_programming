bd1601ea9ae17b93198b8be791496e3950134111
KalenXsosa <j.haywoom@gmail.com>
2023-06-05 20:05:55 -0700

    lol

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: no
+ * @b: ko
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+
+	int crown = *a;
+
+	*a = *b;
+
+	*b = crown;
+}
+
