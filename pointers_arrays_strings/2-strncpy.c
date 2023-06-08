6a132a50aa93a333708ff9825889deec99a3c962
KalenXsosa <j.haywoom@gmail.com>
2023-06-07 17:35:01 -0700

    kikk

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (dest[i] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
-	i++;
 	}
-	while (i < n && src[i] != '\0')
+	while (i < n)
 	{
 	i++;
 	}
