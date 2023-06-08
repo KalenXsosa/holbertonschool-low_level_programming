53b0263b09e5b9e66de7274dcdaa134b3f5046a1
KalenXsosa <j.haywoom@gmail.com>
2023-06-07 18:59:45 -0700

    kkk

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int h = 0;
 
-	while (s1[h] == s2[h] && (s1[h] != '\0' || s2[h] != '\0')
+	while (s1[h] == s2[h] && (s1[h] != '\0' || s2[h] != '\0'))
 	h++;
 	h = s1[h] = s2[h];
 	return (h);
