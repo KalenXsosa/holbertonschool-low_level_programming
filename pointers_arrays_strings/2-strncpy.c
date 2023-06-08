0a9089f07502215245c5693fba7eb80346e2f79a
KalenXsosa <j.haywoom@gmail.com>
2023-06-07 17:25:35 -0700

    lolo

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - check the code
+ * @dest: olo
+ * @src: greg
+ * @n:wq
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+	dest[i] = src[i];
+	i++;
+	}
+	while (i < n && src[i] != '\0')
+	{
+	i++;
+	}
+	return (dest);
+
+}
+
+
