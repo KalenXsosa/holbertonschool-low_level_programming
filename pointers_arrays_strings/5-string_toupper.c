d5bef6f931fafdceb921dbbaac492933bdc91631
KalenXsosa <j.haywoom@gmail.com>
2023-06-08 22:09:22 -0700

    kiiii

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the code
+ * @str: eeee
+ * Return: Always 0.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	str[i] -= 32;
+	i++;
+	}
+	return (str);
+}
