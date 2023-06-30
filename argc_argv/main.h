c464c0d9612f7cfb5cb7b9bb55b1448a3709fa79
KalenXsosa <j.haywoom@gmail.com>
2023-06-30 12:04:13 -0700

    loppp

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,4 +1,4 @@
-int main (int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 /*
  * File: main.h
@@ -45,4 +45,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int main (int argc, char *argv[]);
+
