c26e6a614439c5bac2e52668ecdab003bb9d9c82
KalenXsosa <j.haywoom@gmail.com>
2023-07-01 11:50:17 -0700

    loooooo

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,9 +10,18 @@
 */
 int main(int argc, char *argv[])
 {
-	(void) argv;
+	int n1 = 0, n2 = 0;
 
-	printf("%d\n", argc - 1);
-
+	if (argc == 3)
+	{
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
+	printf("%d\n", n1 * n2);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
 	return (0);
 }
